47c713f8724a67068d405d51e3bf76da64aa7cd7
CodeMaverick <inzera1@gmail.com>
2024-12-25 20:42:07 +0500

    Собрал приложение Календарь, добавил стили

--- FlyFishingSport/views.py
+++ FlyFishingSport/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.shortcuts import render
 from .models import SiteSettings
+from SportCalendar.models import Event
+from django.utils import timezone
 
 def some_view(request):
     site_settings = SiteSettings.objects.first()
@@ -8,7 +9,8 @@ def some_view(request):
     return render(request, 'base.html', {'logo_url': logo_url})
 
 def index(request):
-    return render(request, 'index.html')
+    upcoming_events = Event.objects.filter(date_start__gte=timezone.now()).order_by('date_start')[:5]
+    return render(request, 'index.html', {'upcoming_events': upcoming_events})
 
 def contact_view(request):
     return render(request, 'contact.html')
--- SportCalendar/models.py
+++ SportCalendar/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Club(models.Model):
     name = models.CharField(max_length=255)
