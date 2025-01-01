a3716d9aa5dad9d4113ed3e84274af39d9b2043d
CodeMaverick <inzera1@gmail.com>
2025-01-01 21:35:28 +0500

    исправлен settings.py для корректной работы статик

--- FlyFishingSport/settings.py
+++ FlyFishingSport/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "FlyFishingSport/static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
